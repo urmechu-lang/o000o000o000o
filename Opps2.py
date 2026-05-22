c9d225e93a304c9aa3d0e1befdddc70966742ddd
urmechu-lang <urme.chu@gmail.com>
2026-05-22 19:41:31 -0400

    Add files via upload

---
+++ Opps2.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def __init__(self,max_speed,milage):
+        self.max_speed=max_speed
+        self.milage=milage
+JB=Vehicle(180,90)
+print("THE MAX SPEED IS ",JB.max_speed)
+print("THE MAX MILAGE",JB.milage)
+
+
+
